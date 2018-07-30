fb75648c68bca9c64deb309e98b292c7a89811e3
Matt Tilley <mtilley255@gmail.com>
2018-07-30 12:44:28 -0500

    Added test code / cannot get localhost to come up.

--- my-app/src/Test.vue
+++ my-app/src/Test.vue
@@ -1,9 +1,15 @@
 <template>
-
+    <div id="tester">
+    <h1>TESTING TESTING ONE TWO THREE</h1>
+    </div>
 </template>
 
 
 <script>
+
+export default {
+
+}
 
 </script>
 
--- my-app/src/main.js
+++ my-app/src/main.js
@@ -2,9 +2,15 @@ import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import Test from "./Test.vue"
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+
+new Vue({
+  render: h => h(Test)
+})
